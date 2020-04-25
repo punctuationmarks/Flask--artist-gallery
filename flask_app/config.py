99b61782881247132bc26114c81518dca535f840
punctuationmarks <punctuationmarks@protonmail.com>
2020-04-24 20:47:27 -0400

    refactoring

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,22 +1,12 @@
 import os
 
 class Config:
-    try:
-        SECRET_KEY =  os.environ.get('SECRET_KEY')
-        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-        # you can set key as config
-        GOOGLEMAPS_KEY = os.environ.get('GOOGLEMAPS_KEY')
-        MAIL_SERVER = 'smtp.googlemail.com'
-        MAIL_PORT = 587
-        MAIL_USE_TLS = True
-        MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-        MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    except AttributeError:
-        SECRET_KEY =  "ENTER_SECRET_KEY_HERE"
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-        GOOGLEMAPS_KEY =  "ENTER_SECRET_KEY_HERE"
-        MAIL_PORT = 587
-        MAIL_USE_TLS = True
-        MAIL_SERVER = 'smtp.googlemail.com'
-        MAIL_USERNAME = "SOMETHING@GMAIL.COM"
-        MAIL_PASSWORD = "SOMETHINGTOUGHANDMEMORABLE"
+    SECRET_KEY =  "ENTER_SECRET_KEY_HERE" #  os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' #os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # you can set key as config
+    GOOGLEMAPS_KEY =  "ENTER_SECRET_KEY_HERE" # os.environ.get('GOOGLEMAPS_KEY')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "SOMETHING@GMAIL.COM" # os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = "SOMETHINGTOUGHANDMEMORABLE" # os.environ.get('MAIL_PASSWORD')
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 """
-database not being built? in the virtual environment in python3's cli:
+# database not being built on run? in the virtual environment in python3's cli:
 from flask_app import create_app
 app = create_app()
 app.app_context().push()
